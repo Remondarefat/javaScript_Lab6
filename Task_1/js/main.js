f8173b126db133b62dae68884cd7fee395bd93a3
remonda refat <remondarefat2017@gmail.com>
2023-12-26 23:51:17 +0200

    update_2

--- Task_1/js/main.js
+++ Task_1/js/main.js
@@ -4,31 +4,31 @@ var currentDiv = document.getElementById ("currentDiv");
 var newDiv =document.createElement("p");
 
 
-function checkInput(userInfo){
-  if(userInfo.userName == "admin" & userInfo.userPass == 123){
+function checkInput(){
+  var userInfo = {
+    userName: userNameInput.value,
+    userPass: passwordInput.value,
+  };
+
+  if(userInfo.userName == "admin" & userInfo.userPass == "Remonda@2023"){
     var newContent = document.createTextNode(`WELCOME ${userInfo.userName}`);
     newDiv.appendChild(newContent);
     document.body.appendChild(newDiv , currentDiv).setAttribute("style", "fontWheight: bold; textAlighn: center; color: red;");
 
     // alert("done register");
   }
-  else
-  {
-    var newContent = document.createTextNode(`Not Registered`);
-    newDiv.appendChild(newContent);
-    document.body.appendChild(newDiv , currentDiv).setAttribute("style", "fontWheight: bold; textAlighn: center; color: red;");
 
-    // alert("not register");
-
-  }
+  else if(userInfo.userName !="admin"){ 
+    alert("please enter correct username");
+    // document.getElementById("loginform").username.focus();
+    // document.getElementById("loginform").username.value=" ";
+    
 }
-
-
-function addInput() {
-  var userInfo = {
-    userName: userNameInput.value,
-    userPass: passwordInput.value,
-  };
-  checkInput(userInfo);
+    else { 
+    var passExpr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/;
+    if(passExpr.test(userInfo.userPass.value)==false){
+        alert("check that password should contain small, capital and numbers should atleast 8 charcters");
+        // document.getElementById("loginform").password.focus();
 }
+}}
 
--- task_2/js/main.js
+++ task_2/js/main.js
@@ -4,16 +4,24 @@ var taskInfo = document.getElementById("taskInfo");
 var displayInput =document.getElementById("displayInput");
 var tasksContainer = [];
 
-// function to calculate taska in array and then display it in todolist table
+if (localStorage.getItem("taskValue") != null) {
+  tasksContainer = JSON.parse(localStorage.getItem("taskValue"));
+  displayTasks();
+}
+
+// ! function to add tasks in array and then display it in todolist table
 function addTasks() {
-  tasksContainer.push(taskInfo.value);
-  // call function to display task in table
+  var taskValue = taskInfo.value ;
+  tasksContainer.push(taskValue);
+  //! add tasks in local storage ---
+  localStorage.setItem("taskValue", JSON.stringify(tasksContainer));
+  //?  call function to display task in table
   displayTasks() 
-  // call function to clear the input after add it
+  // ? call function to clear the input after add it
   clearTaskInput();
 }
 
-// function to display task in todo List Table
+// todo: function to display task in todo List Table
 function displayTasks() {
   var cartona = "";
   for (var i = 0; i < tasksContainer.length; i++){
@@ -34,21 +42,23 @@ function displayTasks() {
   document.getElementById("tableData").innerHTML = cartona;
 }
 
-//function to clear Task Input -----
+// ?function to clear Task Input -----
 function clearTaskInput(){
   taskInfo.value="";
 }
 
-//function to done task 
+// ? function to done task 
 function doneMark(i){
   tasksContainer[i] = `${tasksContainer[i]} ---- Done` ;
 
   displayTasks();
 } 
 
-/// function to delete task
+/// ? function to delete task
 function deletTask(taskIndex) {
   tasksContainer.splice(taskIndex, 1);
+  //! Delete from local storage-----
+  localStorage.setItem("taskValue", JSON.stringify(tasksContainer));
   displayTasks();
 }
 
